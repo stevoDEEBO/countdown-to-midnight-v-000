ba8a4d9beab4243da89f84bd774de858cdcc0ea7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-18 18:13:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   while number > 0 do
     puts "#{number} SECOND(S)!"
-    number -=
+    number -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
@@ -13,7 +13,7 @@ def countdown_with_sleep(number)
   while number > 0 do
     puts "#{number} SECOND(S)!"
     sleep(1)
-    number -=
+    number -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
